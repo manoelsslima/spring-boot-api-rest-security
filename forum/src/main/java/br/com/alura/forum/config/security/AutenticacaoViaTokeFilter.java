1e18f29f1e9e8eebe2f2fa8ec0c9b5ac11e3916f
Manoel Lima <manoelsslima@yahoo.com.br>
2022-09-27 13:06:31 -0400

    Authentication of client

--- forum/src/main/java/br/com/alura/forum/config/security/AutenticacaoViaTokeFilter.java
+++ forum/src/main/java/br/com/alura/forum/config/security/AutenticacaoViaTokeFilter.java
@@ -1,5 +1,9 @@
 package br.com.alura.forum.config.security;
 
+import br.com.alura.forum.modelo.Usuario;
+import br.com.alura.forum.repository.UsuarioRepository;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.web.filter.OncePerRequestFilter;
 
 import javax.servlet.FilterChain;
@@ -7,23 +11,43 @@ import javax.servlet.ServletException;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
+import java.util.Optional;
 
 /**
  * Does the authentication using a JWT token.
  */
 public class AutenticacaoViaTokeFilter extends OncePerRequestFilter {
     private TokenAPIService tokenApiService;
+    private UsuarioRepository usuarioRepository;
 
-    public AutenticacaoViaTokeFilter(TokenAPIService tokenApiService) {
+    public AutenticacaoViaTokeFilter(TokenAPIService tokenApiService, UsuarioRepository usuarioRepository) {
         this.tokenApiService = tokenApiService;
+        this.usuarioRepository = usuarioRepository;
     }
 
     @Override
     protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain) throws ServletException, IOException {
         String token = recuperarToken(request);
         boolean valido = this.tokenApiService.isTokenValido(token);
-        System.out.println("Token válido: " + valido);
+        if (valido) {
+            autenticarCliente(token);
+        }
         filterChain.doFilter(request, response);
+    }
+
+    /**
+     *
+     * @param token
+     */
+    private void autenticarCliente(String token) {
+        Long idUsuario = this.tokenApiService.getIdUsuario(token);
+        Optional<Usuario> usuarioOptional = this.usuarioRepository.findById(idUsuario);
+        if (usuarioOptional.isPresent()) {
+            Usuario usuario = usuarioOptional.get();
+            UsernamePasswordAuthenticationToken authentication = new UsernamePasswordAuthenticationToken(usuario, null, usuario.getAuthorities());
+            // informa ao spring que o usuário já se autenticou
+            SecurityContextHolder.getContext().setAuthentication(authentication);
+        }
     }
 
     private String recuperarToken(HttpServletRequest request) {
